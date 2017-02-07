ec8ac3e205681293c6acbc33382168f1d1bede11
Treeless <matt_94101@live.ca>
2017-02-06 19:28:33 -0500

    Finished

--- lab2/main.c
+++ lab2/main.c
@@ -1,3 +1,24 @@
+//---------------------------------------
+// Date Format Outputter (LAB2)
+//
+// Name: main.c
+//
+// Written by: Matthew Rowlandson - February 2017
+//
+// Usage: ./dateformat <args>
+//       eg. ./dateformat -CftY 
+//       eg. ./dateformat -C -f -t -Y
+//
+// Params:
+//    -C : prints out the current time
+//    -f : prints out the full time as day-of-week dd-mm-yyyy hh:mm:ss
+//    -t : prints out the current time of day (hh:mm:ss) e.g. 15:32:02
+//    -Y : prints out the current date out in YYYY-MM-DD format (e.g. 2016-02-02)
+//
+// Description: Outputs the date in the specified format to the screen
+//
+//----------------------------------------
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,32 +29,73 @@
 
 // Prototypes
 int hasArg(const char[], int*, char*[]);
+void printCurrentDate();
+void printYearMonthDay();
+void printTimeOfDay();
+void printFullTime();
 
 //main
 int main (int argc, char *argv[]) {
-  printf("STARTING...\n");
-  printf("Has Arg C: %d \n", hasArg("-C", &argc, argv)); //use the ctime() function to print out the current time. 
-  printf("Has Arg Y: %d \n", hasArg("-Y", &argc, argv)); //print the current date out in YYYY-MM-DD format (e.g. 2016-02-02) 
-  printf("Has Arg t: %d \n", hasArg("-t", &argc, argv)); //print out the current time of day (hh:mm:ss) e.g. 15:32:02
-  printf("Has Arg f: %d \n", hasArg("-f", &argc, argv)); //print out the full time as day-of-week dd-mm-yyyy hh:mm:ss (without using ctime()) 
-  exit(0);
+  //use the ctime() function to print out the current time. 
+  if(hasArg("-C", &argc, argv)){
+    printCurrentDate();
+  }
+  //print the current date out in YYYY-MM-DD format (e.g. 2016-02-02) 
+  if(hasArg("-Y", &argc, argv)){
+    printYearMonthDay();
+  }
 
-  // time_t systemTime;
-  // struct tm *timeinfo; //tm_sec, tm_min, tm_day, tm_mon etc
-  // char buffer [80];
+  //print out the current time of day (hh:mm:ss) e.g. 15:32:02
+  if(hasArg("-t", &argc, argv)){
+    printTimeOfDay();
+  }
 
-  // time(&systemTime);
-
-  // timeinfo = localtime(&systemTime);
-
-  // strftime(buffer, 80, "Now it's %y/%m/%d.", timeinfo);
-  // puts(buffer);
-
+  //print out the full time as day-of-week dd-mm-yyyy hh:mm:ss (without using ctime())
+  if(hasArg("-f", &argc, argv)){
+    printFullTime();
+  }
 
   exit(0);
 }
 
-// Check if the specific argument was given
+void printCurrentDate()   {
+  time_t rawtime = time(0);
+
+  printf("Current local time and date: %s", ctime(&rawtime));
+}
+
+void printYearMonthDay(){
+  time_t rawtime = time(0);
+  struct tm *timeinfo = localtime(&rawtime);
+  char buf[80];
+
+  strftime(buf, sizeof(buf), "%Y-%m-%d", timeinfo);
+
+  printf("YYYY-MM-DD Format: %s \n", buf);
+}
+
+void printTimeOfDay(){
+  time_t rawtime = time(0);
+  struct tm *timeinfo = localtime(&rawtime);
+  char buf[80];
+
+  strftime(buf, sizeof(buf), "%X", timeinfo);
+
+  printf("Time of Day: %s \n", buf);
+}
+
+void printFullTime() {
+  //print out the full time as day-of-week dd-mm-yyyy hh:mm:ss (without using ctime())
+  time_t rawtime = time(0);
+  struct tm *timeinfo = localtime(&rawtime);
+  char buf[80];
+
+  strftime(buf, sizeof(buf), "%A %d-%m-%Y %X", timeinfo);
+
+  printf("Full time: %s \n", buf);
+}
+
+// Check if the specific argument was given as an argument
 int hasArg(const char arg[], int *argc, char* argv[]){
   int paramCounter = 1;
   while(paramCounter < *argc){
